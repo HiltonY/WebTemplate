2e4a3ec3aaf1659c46c9dd9d195895ffabfa6a2b
HiltonY <yxinzju@163.com>
2016-09-11 23:29:02 +0800

    添加Spring Mybatis配置和用户接口

---
+++ src/test/java/TestUserService.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+import com.hiltony.web.model.UserInfo;
+import com.hiltony.web.service.UserService;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.alibaba.fastjson.JSON;
+
+/**
+ * Created by YX on 2016/9/11.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring_config.xml",
+        "classpath:spring-mybatis.xml" })
+public class TestUserService {
+
+    private static final Logger LOGGER = Logger
+            .getLogger(TestUserService.class);
+
+    @Autowired
+    private UserService userService;
+
+
+    @Test
+    public void testQueryById1() {
+        UserInfo userInfo = userService.getUserById(1);
+        LOGGER.info(JSON.toJSON(userInfo));
+    }
+
+    @Test
+    public void testQueryAll() {
+        List<UserInfo> userInfos = userService.getUsers();
+        LOGGER.info(JSON.toJSON(userInfos));
+    }
+
+    @Test
+    public void testInsert() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUname("xiaoming");
+        userInfo.setUnumber(4);
+        int result = userService.insert(userInfo);
+        System.out.println(result);
+    }
+}
