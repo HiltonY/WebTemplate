a7824bdde84083dd23c5849ca22004fb65e798c9
HiltonY <yxinzju@163.com>
2016-09-16 10:11:19 +0800

    添加Spring Mybatis配置和用户接口

--- doc/database.sql
+++ doc/database.sql
@@ -1,3 +1,14 @@
 create database webdemo;
+DROP TABLE IF EXISTS `user_t`;
 
-http://blog.csdn.net/huaishuming/article/details/50751935
+CREATE TABLE `user_t` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `age` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+insert  into `user_t`(`id`,`user_name`,`password`,`age`) values (1,'测试','sfasgfaf',24);
+
+-- http://blog.csdn.net/gebitan505/article/details/44455235/
